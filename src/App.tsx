125712cbd9eb723ace8f00a5ee4c89782f6087b7
ZajoncM <milosz.zajonc@gmail.com>
2022-08-09 13:40:19 +0200

    add examples

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import Globe from "./globe";
+import "./App.css";
+
+const loadGlobe = () => import("./globe");
+const Globe = React.lazy(loadGlobe);
 
 function App() {
+  const [showGlobe, setShowGlobe] = React.useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setShowGlobe(true)}>show</button>
+      {/* {showGlobe ? <Globe /> : null} */}
+
+      <React.Suspense fallback={<div>loading globe...</div>}>
+        {showGlobe ? <Globe /> : null}
+      </React.Suspense>
     </div>
   );
 }
