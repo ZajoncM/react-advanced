f65f100d848a159337f80ee79e1cb886f9a3bdc1
ZajoncM <milosz.zajonc@gmail.com>
2022-08-13 19:46:02 +0200

    test: create test for check counter incrementation

--- src/Counter/Counter.test.tsx
+++ src/Counter/Counter.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Counter from "./Counter";
 
 describe("Counter", () => {
@@ -7,5 +8,16 @@ describe("Counter", () => {
 
     const container = screen.getByText(/example counter/);
     expect(container).toBeInTheDocument();
+  });
+
+  it("should increase counter after button clicking", () => {
+    render(<Counter />);
+
+    const display = screen.getByText(/0/);
+    const button = screen.getByRole("button");
+
+    userEvent.click(button);
+
+    expect(display.innerHTML).toContainEqual("1");
   });
 });
