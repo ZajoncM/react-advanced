f7c6693822088a9c6e7f01ae7a3dddd8117ac906
ZajoncM <milosz.zajonc@gmail.com>
2022-08-13 19:48:28 +0200

    feat: implement counter incrementation handling

--- src/Counter/Counter.tsx
+++ src/Counter/Counter.tsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
+
 const Counter = () => {
+  const [counter, setCounter] = useState(0);
   return (
     <div>
       <h1>example counter</h1>
+      <p>{counter}</p>
+      <button onClick={() => setCounter(counter + 1)}>click</button>
     </div>
   );
 };
